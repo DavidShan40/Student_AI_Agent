4db6c708a2ccb02398c8b0b7aec0937f92e8a74f
TAO (DAVID) SHAN <shan2000814@gmail.com>
2023-09-19 14:29:25 -0400

    Create test_mic.py

---
+++ pages/test_mic.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from audiorecorder import audiorecorder
+
+st.title("Audio Recorder")
+audio = audiorecorder("Click to record", "Click to stop recording")
+
+if not audio.empty():
+    # To play audio in frontend:
+    st.audio(audio.export().read())  
+
+    # To save audio to a file, use pydub export method:
+    audio.export("audio.wav", format="wav")
+
+    # To get audio properties, use pydub AudioSegment properties:
+    st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
